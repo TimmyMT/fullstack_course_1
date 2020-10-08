3ef1d96938693eb0c279c3755cb50aacb599d48a
Timmy_MT <mathcore_detected@mail.ru>
2020-10-08 12:38:45 +0300

    added another solutions for even numbers

--- part_1/even_numbers.js
+++ part_1/even_numbers.js
@@ -1,3 +1,15 @@
 for (i = 0; i <= 20; i++) {
     i % 2 === 0 ? console.log(i) : null
 }
+
+let i = 0;
+while (i <= 20) {
+    i % 2 === 0 ? console.log(i) : null
+    i++
+}
+
+let i = 0;
+do {
+    i++
+    i % 2 === 0 ? console.log(i) : null
+} while (i <= 20)
