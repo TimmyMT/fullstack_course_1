e480727255681e062c70e9444a502bfb826eb73d
Timmy_MT <mathcore_detected@mail.ru>
2020-10-10 19:58:43 +0300

    added solution for task #5

---
+++ part_2/chaining_array.js
@@ -0,0 +1,73 @@
+// Выполнить преборазования с массивом сотрудниов компании
+
+function mapSalaries(item) { return item.salary }
+
+function averageValue(values) {
+    let error = null
+    if (!Array.isArray(values)) { error = 'is not array' }
+    values.forEach(element => {
+        if (typeof element != 'number') { error = 'invalid number arguments' }
+    });
+
+    if (error != null) { return error }
+    
+    let all_values = 0
+    values.forEach(element => { all_values += element });
+    let average = all_values / values.length
+
+    return average;
+}
+
+function descSalary(a, b) {
+    if (a.salary < b.salary) {
+        return 1
+    } else if (a.salary > b.salary) {
+        return -1
+    } else {
+        return 0
+    }
+}
+
+function bySalary(item) {
+    return item.salary > 5000
+}
+
+function byAge(item) {
+    return item.age > 25
+}
+
+let employers = [
+    {
+        firstName: 'Alex',
+        lastName: 'Smith',
+        age: 54,
+        salary: 10000,
+        position: 'Architect'
+    },
+    {
+        firstName: 'Gustav',
+        lastName: 'Andersen',
+        age: 31,
+        salary: 5000,
+        position: 'Software engineer'
+    },
+    {
+        firstName: 'Liz',
+        lastName: 'Taylor',
+        age: 20,
+        salary: 7000,
+        position: 'Manager'
+    }
+]
+
+// 1. Узнать среднюю зарплату сотрудников
+let salaries = employers.map(mapSalaries)
+let averageSalaries = averageValue(salaries)
+
+console.log('Средняя зарплата сотрудников: ', averageSalaries)
+
+// 2. Отсортировать сотрудников по зарплате
+// 3. Получить список сотрудников с зарплатой >4500 и возрастом > 25 лет
+let sortedEmployers = employers.sort(descSalary).filter(bySalary).filter(byAge)
+
+console.log(sortedEmployers)
