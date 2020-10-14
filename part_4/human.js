7d519125f689fff7e6bded70efef75fe45405d9c
Timmy_MT <mathcore_detected@mail.ru>
2020-10-14 16:00:51 +0300

    added colution for task №3

---
+++ part_4/human.js
@@ -0,0 +1,87 @@
+// Организовать такую цепочку прототипов, в которой свойства и методы оптимально распределены по объектам
+
+const human = function(person) {
+    this.getPersonData = function() {
+        var personData = {
+            name: person.name,
+            lastName: person.lastName,
+            location: person.location,
+            phoneNumber: person.phoneNumber
+        }
+        return console.log(personData)
+    }
+    this.eat = function() {
+        return console.log(person.name, person.eat())
+    }
+    this.sleep = function() {
+        return console.log(person.name, person.sleep())
+    }
+    this.callFriend = function() {
+        return console.log(person.name, person.callFriend())
+    }
+}
+
+const employee = function(person) {
+    this.getEmployeeData = function() {
+        var employeeData = {
+            position: person.position,
+            startDate: person.startDate,
+            endDate: person.endDate,
+            baseSalary: person.baseSalary,
+            salaryCurrency: person.salaryCurrency,
+            department: person.department
+        }
+        return console.log(employeeData)
+    }
+}
+
+const currentEmployee = function(person) {
+    this.writeReport = function() {
+        return console.log(person.name, person.writeReport())
+    },
+    this.organizeMeeting = function () {
+        return console.log(person.name, person.organizeMeeting())
+    },
+    this.startVacation = function() {
+        return console.log(person.name, person.startVacation())
+    }
+}
+
+const retiredEmployee = function(person) {
+    this.retire = function() {
+        return console.log(person.name, person.retire())
+    }
+}
+
+const john = {
+    name: "John",
+    lastName: "Smith",
+    position: "Senior engineer",
+    startDate: "10.10.1990",
+    endDate: "10.10.2000",
+    baseSalary: "10000",
+    salaryCurrency: "$",
+    location: "Russia",
+    department: "IT",
+    phoneNumber: "+1234567890",
+    eat: function() { return 'eat' },
+    sleep: function() { return 'sleep' },
+    callFriend: function() { return 'call friend' },
+    writeReport: function() { return 'write report' },
+    organizeMeeting: function () { return 'organize meet' },
+    retire: function () { return 'is retired' },
+    startVacation: function () { return 'start vacation' }
+}
+
+employee.prototype = new human(john)
+currentEmployee.prototype = new employee(john)
+retiredEmployee.prototype = new employee(john)
+
+var myEmployee = new currentEmployee(john)
+myEmployee.getPersonData()
+myEmployee.eat()
+myEmployee.organizeMeeting()
+var nowRetiredEmployee = new retiredEmployee(john)
+nowRetiredEmployee.getEmployeeData()
+nowRetiredEmployee.callFriend()
+nowRetiredEmployee.retire()
